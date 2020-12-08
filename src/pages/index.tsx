684619980788061c247dc3b4c474de9dc84575c4
Rene Zorzi <31076613+rzdev97@users.noreply.github.com>
2020-12-08 17:14:21 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,6 +20,49 @@ export default function Index() {
           <SocialList />
         </div>
       </div>
+       <div className="container">
+      <div>
+        <h2> Highlight Projects</h2>
+        <Container>
+          <Row>
+      <Col><Card className="bg-dark text-white">
+  <Card.Img src="holder.js/100px270" alt="Card image" />
+  <Card.ImgOverlay>
+    <Card.Title>Card title</Card.Title>
+    <Card.Text>
+      This is a wider card with supporting text below as a natural lead-in to
+      additional content. This content is a little bit longer.
+    </Card.Text>
+    <Card.Text>Last updated 3 mins ago</Card.Text>
+  </Card.ImgOverlay>
+</Card></Col>
+      <Col><Card className="bg-dark text-white">
+  <Card.Img src="holder.js/100px270" alt="Card image" />
+  <Card.ImgOverlay>
+    <Card.Title>Card title</Card.Title>
+    <Card.Text>
+      This is a wider card with supporting text below as a natural lead-in to
+      additional content. This content is a little bit longer.
+    </Card.Text>
+    <Card.Text>Last updated 3 mins ago</Card.Text>
+  </Card.ImgOverlay>
+</Card></Col>
+      <Col><Card className="bg-dark text-white">
+  <Card.Img src="holder.js/100px270" alt="Card image" />
+  <Card.ImgOverlay>
+    <Card.Title>Card title</Card.Title>
+    <Card.Text>
+      This is a wider card with supporting text below as a natural lead-in to
+      additional content. This content is a little bit longer.
+    </Card.Text>
+    <Card.Text>Last updated 3 mins ago</Card.Text>
+  </Card.ImgOverlay>
+</Card></Col>
+    </Row>
+</Container>
+         </div>
+      </div>
+      
       <style jsx>{`
         .container {
           display: flex;
