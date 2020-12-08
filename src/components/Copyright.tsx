9ec78c34f379461fcd5da52bf0cc778d9778be44
Rene Zorzi <31076613+rzdev97@users.noreply.github.com>
2020-12-08 16:05:53 +0100

    Update Copyright.tsx

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -1,6 +1,35 @@
 export default function Copyright() {
   return (
     <>
+      <Container>
+  
+  <Row>
+    <Col><Nav defaultActiveKey="/home" className="flex-column">
+  <Nav.Link href="/home">Active</Nav.Link>
+  <Nav.Link eventKey="link-1">Link</Nav.Link>
+  <Nav.Link eventKey="link-2">Link</Nav.Link>
+  <Nav.Link eventKey="disabled" disabled>
+    Disabled
+  </Nav.Link>
+</Nav></Col>
+    <Col><Nav defaultActiveKey="/home" className="flex-column">
+  <Nav.Link href="/home">Active</Nav.Link>
+  <Nav.Link eventKey="link-1">Link</Nav.Link>
+  <Nav.Link eventKey="link-2">Link</Nav.Link>
+  <Nav.Link eventKey="disabled" disabled>
+    Disabled
+  </Nav.Link>
+</Nav></Col>
+    <Col><Nav defaultActiveKey="/home" className="flex-column">
+  <Nav.Link href="/home">Active</Nav.Link>
+  <Nav.Link eventKey="link-1">Link</Nav.Link>
+  <Nav.Link eventKey="link-2">Link</Nav.Link>
+  <Nav.Link eventKey="disabled" disabled>
+    Disabled
+  </Nav.Link>
+</Nav></Col>
+  </Row>
+</Container>
       <p>&copy; 2020</p>
       <style jsx>
         {`
