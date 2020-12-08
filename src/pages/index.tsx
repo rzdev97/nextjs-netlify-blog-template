e49b3bba296863bed44e95dc98bcf088509eac33
Rene Zorzi <31076613+rzdev97@users.noreply.github.com>
2020-12-08 15:34:49 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,7 +27,7 @@ export default function Index() {
           justify-content: center;
           flex: 1 1 auto;
           padding: 0 1.5rem;
-          background-color: #0B090A;
+          background-color: #F5F3F4;
         }
         h1 {
           font-size: 2.5rem;
